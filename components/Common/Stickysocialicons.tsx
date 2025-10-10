dc30544ecc809253053d7e4f3e55035dd68b7c0b
Dhanya Deepika Yalagana <dhanyadeepikayalagana489@gmail.com>
2025-10-10 19:18:32 +0530

    Ready for production deployment

--- components/Common/Stickysocialicons.tsx
+++ components/Common/Stickysocialicons.tsx
@@ -5,42 +5,67 @@ import Image from "next/image";
 
 const StickySocialIcons: React.FC = () => {
   return (
-      <div
-        className="fixed top-1/3 right-2 md:right-3 flex flex-col space-y-3 z-50"
-        id="social-links"
-      >
+    <div className="fixed bottom-3.5 right-2 md:right-3 flex flex-col space-y-3 z-50">
+      {/* Instagram */}
+      <Image
+        src="/images/instagram-icon.svg"
+        width={37}
+        height={37}
+        alt="instagram"
+        className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
+        onClick={() =>
+          window.open("https://www.instagram.com/farmnatura.in", "_blank")
+        }
+      />
 
+      {/* Facebook */}
+      <Image
+        src="/images/facebook-icon.svg"
+        width={37}
+        height={37}
+        alt="facebook"
+        className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
+        onClick={() =>
+          window.open("https://www.facebook.com/farmnatura.in", "_blank")
+        }
+      />
+
+      {/* YouTube */}
+      <Image
+        src="/images/youtube-icon.svg"
+        width={37}
+        height={37}
+        alt="youtube"
+        className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
+        onClick={() =>
+          window.open("https://www.youtube.com/@FarmNatura", "_blank")
+        }
+      />
+
+      {/* WhatsApp */}
+      <a
+        href="https://api.whatsapp.com/send?phone=919100007368"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="/images/whatsapp-icon.svg"
+          alt="whatsapp"
+          className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
+        />
+      </a>
+
+      {/* Phone */}
+      <a href="tel:919579555666">
         <Image
-          src="/images/instagram-icon.svg"
+          src="/images/phone-icon.svg"
+          alt="phone"
           width={37}
           height={37}
-          alt="instagram"
           className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
-          onClick={() =>
-            window.open("https://www.instagram.com/farmnatura.in", "_blank")
-          }
         />
-        <Image
-          src="/images/facebook-icon.svg"
-          width={37}
-          height={37}
-          alt="facebook"
-          className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
-          onClick={() =>
-            window.open("https://www.facebook.com/farmnatura.in", "_blank")
-          }
-        />
-        <Image
-          src="/images/youtube-icon.svg"
-          width={37}
-          height={37}
-          alt="youtube"
-          className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
-          onClick={() =>
-            window.open("https://www.youtube.com/@FarmNatura", "_blank")
-          }
-        />
-      </div> 
+      </a>
+    </div>
   );
 };
 
--- components/project-highlights/FarmNaturaFooter.tsx
+++ components/project-highlights/FarmNaturaFooter.tsx
@@ -135,31 +135,7 @@ const FarmNaturaFooter: React.FC<FarmNaturaFooterProps> = ({ bgColor }) => {
         </div>
       </div>
 
-      {/* Floating Bottom-Right Social Icons */}
-      <div className="fixed bottom-3.5 right-2 md:right-3 flex flex-col space-y-3 z-50">
-        <a
-          href="https://api.whatsapp.com/send?phone=919100007368"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            src="/images/whatsapp-icon.svg"
-            alt="whatsapp"
-            width={37}
-            height={37}
-            className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
-          />
-        </a>
-        <a href="tel:919579555666">
-          <Image
-            src="/images/phone-icon.svg"
-            alt="phone"
-            width={37}
-            height={37}
-            className="w-[25px] h-[25px] md:w-[37px] md:h-[37px] cursor-pointer"
-          />
-        </a>
-      </div>
+      
     </footer>
   );
 };
